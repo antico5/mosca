042a93cb271960cd1117e0fb6719a06821ed9d33
Pablo Oldani <oldani.pablo@gmail.com>
2014-08-06 20:22:07 -0300

    Add test results.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
 require "codeclimate-test-reporter"
-ENV['CODECLIMATE_REPO_TOKEN']='188526a5386ffb1697df439556a65c1f1ab2f7889da7452c04220432549248eb'
 CodeClimate::TestReporter.start
 require 'mosca'
 require 'client_double'
