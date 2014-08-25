0669efe9f95d4c62bdb530d94185345dc422195b
Armando Andini <armando.andini@hotmail.com>
2014-08-25 11:50:43 -0300

    Refactor the specs for mosca to use :let instead of instance variables.

--- spec/mosca_spec.rb
+++ spec/mosca_spec.rb
@@ -3,40 +3,43 @@ require 'spec_helper'
 describe Mosca do
   OUT = "out_topic"
   IN = "in_topic"
+  MESSAGE = "test_message"
 
-  before do
-    @client_double = ClientDouble.new
-    @mosca = Mosca::Client.new topic_out: OUT, topic_in: IN, client: @client_double
-    @test_message = "something"
-  end
+  let (:client) {
+    ClientDouble.new
+  }
+
+  let (:mosca) {
+    Mosca::Client.new topic_out: OUT, topic_in: IN, client: client
+  }
 
   it "has a default broker" do
-    expect(@mosca.broker).to eq("test.mosquitto.org")
+    expect(mosca.broker).to eq("test.mosquitto.org")
   end
 
   it "has attribute accessors" do
     %w{user pass broker topic_in topic_out topic_base}.each do |attr|
-      expect(@mosca).to respond_to(attr.to_sym)
-      expect(@mosca).to respond_to("#{attr}=".to_sym)
+      expect(mosca).to respond_to(attr.to_sym)
+      expect(mosca).to respond_to("#{attr}=".to_sym)
     end
   end
 
   describe "publishing" do
 
     it "uses topic_out to publish if it was specified, and publishes the desired message" do
-      expect(@client_double).to receive(:publish).with(OUT,@test_message)
-      @mosca.publish @test_message
+      expect(client).to receive(:publish).with(OUT,MESSAGE)
+      mosca.publish MESSAGE
     end
 
     it "can take a topic_out as argument when publishing" do
       new_out_topic = "other_out_topic"
-      expect(@client_double).to receive(:publish).with(new_out_topic,@test_message)
-      @mosca.publish @test_message, topic_out: new_out_topic
+      expect(client).to receive(:publish).with(new_out_topic,MESSAGE)
+      mosca.publish MESSAGE, topic_out: new_out_topic
     end
 
     it "should wait for a response on topic_in if it's specified" do
-      expect(@client_double).to receive(:connect).once.and_yield(@client_double)
-      expect(@mosca.publish(@test_message, response: true)).to eq("response")
+      expect(client).to receive(:connect).once.and_yield(client)
+      expect(mosca.publish(MESSAGE, response: true)).to eq("response")
     end
 
   end
@@ -44,18 +47,18 @@ describe Mosca do
   describe "subscribing" do
 
     it "uses topic_in to get messages if it was specified" do
-      expect(@client_double).to receive(:get).with(IN)
-      @mosca.get
+      expect(client).to receive(:get).with(IN)
+      mosca.get
     end
 
     it "can take a topic_in as argument when getting" do
       new_in_topic = "other_in_topic"
-      expect(@client_double).to receive(:get).with(new_in_topic)
-      @mosca.get topic_in: new_in_topic
+      expect(client).to receive(:get).with(new_in_topic)
+      mosca.get topic_in: new_in_topic
     end
 
     it "will receive the message with get" do
-      expect(@mosca.get).to eq("response")
+      expect(mosca.get).to eq("response")
     end
 
   end
@@ -63,11 +66,11 @@ describe Mosca do
   describe "parsing the incoming messages" do
 
     it "gets a hash if the message was a JSON object" do
-      expect(@mosca.get topic_in: "json_in_topic").to be_a(Hash)
+      expect(mosca.get topic_in: "json_in_topic").to be_a Hash
     end
 
     it "gets an array if the message was a JSON array" do
-      expect(@mosca.get topic_in: "json_array_in_topic").to be_a(Array)
+      expect(mosca.get topic_in: "json_array_in_topic").to be_a Array
     end
 
   end
@@ -75,14 +78,14 @@ describe Mosca do
   describe "formatting the topic names" do
 
     before do
-      @mosca.topic_base = "/base/"
+      mosca.topic_base = "/base/"
     end
 
     it "uses topic_base to form the full topic for publishing and receiving" do
-      expect(@client_double).to receive(:get).with("/base/#{IN}")
-      expect(@client_double).to receive(:publish).with("/base/#{OUT}", @test_message)
-      @mosca.get
-      @mosca.publish @test_message
+      expect(client).to receive(:get).with("/base/#{IN}")
+      expect(client).to receive(:publish).with("/base/#{OUT}", MESSAGE)
+      mosca.get
+      mosca.publish MESSAGE
     end
 
   end
